144522cad3df0bf755d0ac9b250a4302159c615d
Sam Avery <sam.avery1@gmail.com>
2022-09-11 10:33:04 -0500

    commit tree c02087d597d9b4ff41a66e1e6b3b0d1c3124e740

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+// const totalBatteries = batteryBatches.reduce(function(countObj, currentNumber) {
+//     if (currentNumber in countObj) {
+//       countObj[currentLetter]++;
+//     } else {
+//       countObj[currentLetter] = 1;
+//     }
+//     return countObj;
+//   }, {});
+
+
+  const totalBatteries = batteryBatches.reduce(function(accumulator, element) {
+    return element + accumulator
+  })
